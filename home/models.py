5ac4d8c179edbfd7c2e4545626e06c0ffe2973d8
chikadim <>
2023-10-19 00:27:33 +0000

    Install summernote and link it to content field

--- home/models.py
+++ home/models.py
@@ -35,6 +35,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     '''This model handles comments'''
+
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
                              related_name="comments")
     name = models.CharField(max_length=80)
