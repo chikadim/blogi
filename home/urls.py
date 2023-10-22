744e36118de754e0660d99c31d90cf3dfd865396
chikadim <>
2023-10-22 00:22:21 +0000

    Deployment commit

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetails.as_view(), name='post_details'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
--- static/js/script.js
+++
@@ -1,1 +0,0 @@
-document.getElementById('test').style.color = "green";
