22bcfbbe8ffbfe0e4ee4df125d4dacd347f10010
Chika Dim <chikadim@chikas-mbp.speedport.ip>
2023-10-25 01:58:42 +0200

    Fix post image not displaying

--- home/admin.py
+++ home/admin.py
@@ -5,8 +5,7 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
-    list_display = ('title', 'slug', 'status', 'created_on')
+    list_display = ('title', 'featured_image', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
