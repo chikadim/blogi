f0e657ed1caca46e8ecd3c13e30da338d392508c
Chika Dim <chikadim@chikas-mbp.speedport.ip>
2023-10-24 02:26:05 +0200

    Make add blogs view and make a post successfully

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path("add_blogs/", views.add_blogs, name="add_blogs"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 '''Import model'''
+from django.shortcuts import render
 from django.views import generic
 from .models import Post
+from home.forms import BlogPostForm
 
 
 class PostList(generic.ListView):
@@ -9,3 +11,18 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "home/index.html"
     paginate_by = 6
+
+
+def add_blogs(request):
+    if request.method == "POST":
+        form = BlogPostForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            blogpost = form.save(commit=False)
+            blogpost.author = request.user
+            blogpost.save()
+            obj = form.instance
+            alert = True
+            return render(request, "home/add_blogs.html", {'obj': obj, 'alert': alert})
+    else:
+        form = BlogPostForm()
+    return render(request, "home/add_blogs.html", {'form': form})
