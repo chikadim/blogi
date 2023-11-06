fe338dadbb4d84abc3a04130fbff54f312c1415b
Chika Dim <chikadim@chikas-mbp.speedport.ip>
2023-11-06 04:53:02 +0100

    Add function to automatically create profile

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 '''Import model'''
 from django.shortcuts import render, redirect
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 from django.views import generic
 from django.views.generic import UpdateView, ListView
 from .models import Post, Profile, Variety
@@ -113,3 +116,12 @@ def edit_profile(request):
     else:
         form = ProfileForm(instance=profile)
     return render(request, "home/edit_profile.html", {'form': form})
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    try:
+        if created:
+            Profile.objects.create(user=instance).save()
+    except Exception as err:
+        print(f'Error creating user profile!\n{err}')
